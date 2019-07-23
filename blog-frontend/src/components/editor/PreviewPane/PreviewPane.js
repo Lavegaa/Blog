3a049f2552a79afd2bac7def262a7752f905f954
ChanhoKim <cksgh405@gmail.com>
2019-07-23 15:26:24 +0900

    리덕스를 사용한 상태관리 editor에서 작성할 제목, 내용, 태그들의 상태를 관리 markdown을 previewPane과 연동 prismjs를 이용한 마크다운 스타일링

--- blog-frontend/src/components/editor/PreviewPane/PreviewPane.js
+++ blog-frontend/src/components/editor/PreviewPane/PreviewPane.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import styles from './PreviewPane.scss';
 import classNames from 'classnames/bind';
+import MarkdownRender from 'components/common/MarkdownRender';
 
 const cx = classNames.bind(styles);
 
 const PreviewPane = ({ markdown, title }) =>  (
     <div className={cx('preview-pane')}>    
-        <h1 className={cx('title')}>제목</h1>
+        <h1 className={cx('title')}>
+            {title}
+        </h1>
         <div>
-            내용
+            <MarkdownRender markdown={markdown}/>
         </div>
     </div>
 );
