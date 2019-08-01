17c08fec913b7e25903b7e87af0d0b93cc7fda38
ChanhoKim <cksgh405@gmail.com>
2019-08-01 16:50:49 +0900

    로그인 구현을 위한 koa-session라이브러리 설치

--- blog-backend/src/index.js
+++ blog-backend/src/index.js
@@ -8,9 +8,13 @@ const api = require('./api');
 
 const mongoose = require('mongoose');
 
+const session = require('koa-session');
+
+
 const {
     PORT:port = 4000,   //값이 존재하지 않을때 4000값을 기본으로 사용
-    MONGO_URI:mongoURI
+    MONGO_URI:mongoURI,
+    COOKIE_SIGN_KEY: signKey
 } = process.env;
 
 const app = new Koa();
@@ -28,6 +32,16 @@ router.use('/api', api.routes());
 
 //라우터 적용 전에 bodyparser 적용
 app.use(bodyParser());
+
+
+//세션/키 적용
+const sessionConfig = {
+  maxAge: 8640000,  //하루
+  //signed: true(기본으로 설정되어 있습니다.)
+}
+
+app.use(session(sessionConfig, app));
+app.key = [signKey];
 
 //app인스턴스에 라우터 적용
 app.use(router.routes()).use(router.allowedMethods());
